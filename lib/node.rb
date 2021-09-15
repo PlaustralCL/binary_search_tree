c8776338924f6f8d77f47c9d556c41031eb22a2c
PlaustralCL <plaustralcl@gmail.com>
2021-09-14 21:10:43 -0400

    Initial commit

---
+++ lib/node.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Stores individual nodes for the binary search tree
+class Node
+
+end
---
+++ lib/tree.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "node"
+
+# Builds and maintains a binary search tree
+class Tree
+
+end
