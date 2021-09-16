b273d05c2eaf91f4724f63618756abe6b4670077
PlaustralCL <plaustralcl@gmail.com>
2021-09-15 20:57:07 -0400

    Add a working #build_tree method

--- lib/node.rb
+++ lib/node.rb
@@ -2,5 +2,18 @@
 
 # Stores individual nodes for the binary search tree
 class Node
+  include Comparable
+
+  attr_accessor :data, :left_node, :right_node
+
+  def initialize(data: nil, left_node: nil, right_node: nil)
+    @data = data
+    @left_node = left_node
+    @right_node = right_node
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
 
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
+require "pry"
 require_relative "node"
 
 # Builds and maintains a binary search tree
 class Tree
+  attr_accessor :root
 
+  def initialize(initial_array)
+    @root = build_tree(initial_array)
+  end
+
+  def build_tree(array)
+    return Node.new(data: array.first) if array.length == 1
+    return nil if array.empty?
+
+    array = array.uniq.sort
+    middle = (array.length / 2)
+
+    root = Node.new(data: array[middle])
+    root.left_node = build_tree(array[0..middle - 1])
+    root.right_node = build_tree(array[middle + 1..-1])
+    @root = root
+  end
 end
+
+
+test_array = [1, 2, 3, 4, 5]
+tree = Tree.new(test_array)
+p tree.root
